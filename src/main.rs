d735f7adc9047cc403d1a119881a075cadc8cde0
Nicholas Hassan <nick@alphor.au>
2024-01-15 18:58:14 +1030

    use naivedatetime

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
 use systemd::journal;
+use chrono::{NaiveDateTime};
+use lazy_static::lazy_static;
+
+lazy_static!(
+	// system timezone
+	static ref LOCAL_TZ_OFFSET: i64 = chrono::offset::Local::now().offset().local_minus_utc() as i64;
+);
 
 fn print_a_message(entry: Result<Option<journal::JournalRecord>,systemd::Error>) {
 	match entry {
 		Ok(Some(entry)) => 
 		{
-			// println!("{} {} {}", entry.get("_SOURCE_REALTIME_TIMESTAMP").unwrap(), entry.get("_COMM").unwrap(), entry.get("MESSAGE").unwrap());
-			println!("{}: {}", entry.get("_COMM").unwrap(), entry.get("MESSAGE").unwrap());
+			let timestamp = match entry.get("_SOURCE_REALTIME_TIMESTAMP") {
+				Some(t) => {
+					let t = t.parse::<u64>().unwrap();
+					let t = t / 1000000; // convert from ns to seconds
+					let t = t as i64;
+					let t = match NaiveDateTime::from_timestamp_opt(t + (*LOCAL_TZ_OFFSET), 0) {
+						Some(t) => t,
+						None => {
+							// now time
+							chrono::offset::Local::now().naive_local()
+						},
+					};
+					let t = t.format("%b %d %H:%M:%S");
+					t.to_string()
+				},
+				None => {
+					// do it in format Jan 15 18:36:55
+					chrono::offset::Local::now().format("%b %d %H:%M:%S").to_string()
+				},
+			};
+
+			let identifier = match entry.get("SYSLOG_IDENTIFIER") {
+				Some(i) => i,
+				None => "unknown",
+			};
+
+			let message = match entry.get("MESSAGE") {
+				Some(m) => m,
+				None => "unknown",
+			};
+
+			println!("[{}] {}: {}", timestamp, identifier, message);
 			// println!("dump: {:#?}", entry);
 		},
 		Ok(None) => {
